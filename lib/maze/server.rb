8d8b53020ec0f3157912b4477cf2e1b90165f884
Peter Schröder <phoetmail@googlemail.com>
2012-11-15 12:48:26 +0100

    move the event handling into a class and delegate

--- lib/maze/server.rb
+++ lib/maze/server.rb
@@ -1,43 +1,44 @@
 require 'gserver'
 
 module Maze
-  class Server < GServer
-    attr_reader :options
+  class Server
+    attr_accessor :queue
+    attr_reader :options, :event_endpoint
 
-    def initialize options
-      super EVENT_SOURCE_PORT
-      self.audit = !!options[:verbose]
+    def initialize options = { verbose: false }
       @options = options
+      @queue   = []
+
+      @event_endpoint = EventEndpoint.new self
+      @event_endpoint.audit = !!options[:verbose]
     end
 
-    def serve io
-      while payload = io.readline
-        log "received payload: #{payload}"
-        self.class.queue << payload
-      end
+    def start
+      event_endpoint.start
+    end
+
+    def stop
+      event_endpoint.shutdown
     end
 
     def log message
       puts message if options[:verbose]
     end
+  end
 
-    def self.queue
-      @@_queue ||= []
+  class EventEndpoint < GServer
+    attr_reader :server
+
+    def initialize server
+      super EVENT_SOURCE_PORT
+      @server = server
     end
 
-    def self.start options = { verbose: false }
-      @server.stop if @server
-
-      @server = Server.new options
-      @server.start
-    end
-
-    def self.running?
-      !@server.stopped?
-    end
-
-    def self.stop
-      @server.shutdown if running?
+    def serve io
+      while payload = io.readline
+        server.log "received payload: #{payload}"
+        server.queue << payload
+      end
     end
   end
 end
--- spec/maze_spec.rb
+++ spec/maze_spec.rb
@@ -2,19 +2,22 @@ require 'spec_helper'
 
 describe Maze do
   context "running client and server" do
+    let(:event_source) { Maze::EventSource.new({ count: 3, delay: 0 }) }
+    let(:server) { Maze::Server.new }
+
     before do
-      Maze::Server.start
+      server.start
     end
 
     after do
-      Maze::Server.stop
+      server.stop
     end
 
     it "emits events through the client and they are queued on the server" do
-      Maze::Server.queue.should have(0).elements
-      Maze::EventSource.new({ count: 3, delay: 0 }).emit_events
+      server.queue.should have(0).elements
+      event_source.emit_events
       sleep(1)
-      Maze::Server.queue.should have(3).elements
+      server.queue.should have(3).elements
     end
   end
 end
